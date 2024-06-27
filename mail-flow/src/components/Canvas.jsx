c438e8fc557f2d22c3a3916ffb3331c9fcd77b6f
Kawalmeet Singh <kmssr782002@gmail.com>
2024-06-28 01:17:16 +0530

    Added a dropdown \for new nodes

--- mail-flow/src/components/Canvas.jsx
+++ mail-flow/src/components/Canvas.jsx
@@ -1,80 +1,99 @@
-// Canvas.js
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import ReactFlow, {
-  MiniMap,
-  Controls,
-  Background,
-  useNodesState,
-  useEdgesState,
   addEdge,
+  Background,
+  Controls,
+  MiniMap,
+  useEdgesState,
+  useNodesState,
 } from 'reactflow';
-import PaymentProvider from "./nodes/TestNode";
+import 'reactflow/dist/style.css';  // Make sure to import the React Flow styles
 import CustomEdge from './CustomEdge';
-// import ColdMailNode from './Nodes/ColdMailNode';
+import ColdMailNode from './nodes/ColdMailNode';
+import { initialEdges, initialNodes } from './Constants';
+import Dropdown from './Dropdown';
+import LeadSourceNode from './nodes/LeadSourceNode';
+import DelayNode from './nodes/DelayNode';
 
-
+// Define node types and edge types after importing components
 const nodeTypes = {
-    paymentProvider: PaymentProvider,
-  };
-  const edgeTypes = {
-    customEdge: CustomEdge,
-  };
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
-];
-
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+  lead: LeadSourceNode,
+  delay : DelayNode,
+  coldMail: ColdMailNode,
+};
+const edgeTypes = {
+  customEdge: CustomEdge,
+};
 
 const Canvas = () => {
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  
-    const onConnect = useCallback(
-      (connection) => {
-        const edge = {
-          ...connection,
-          animated: true,
-          id: `${edges.length} + 1`,
-          type: "customEdge",
-        };
-        setEdges((prevEdges) => addEdge(edge, prevEdges));
-      },
-      [edges]
-    );
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
+  const onConnect = useCallback(
+    (connection) => {
+      const edge = {
+        ...connection,
+        animated: true,
+        id: `${edges.length + 1}`,
+        type: 'customEdge',
+      };
+      setEdges((prevEdges) => addEdge(edge, prevEdges));
+    },
+    [edges, setEdges]
+  );
+
+  const addDelay = () => {
+    const newNode = {
+      id: (nodes.length + 1).toString(),
+      position: { x: 0, y: 200 },
+      data: { label: 'Delay' },
+      type: 'delay', 
+    };
+
+    setNodes((prevNodes) => [...prevNodes, newNode]);
+  };
+  const addLead = () => {
+    const newNode = {
+      id: (nodes.length + 1).toString(),
+      position: { x: 0, y: 200 },
+      data: { label: 'Lead' },
+      type: 'lead', 
+    };
+
+    setNodes((prevNodes) => [...prevNodes, newNode]);
+  };
   const addColdMailNode = () => {
     const newNode = {
       id: (nodes.length + 1).toString(),
       position: { x: 0, y: 200 },
       data: { label: 'Cold Email' },
+      type: 'coldMail', 
     };
 
     setNodes((prevNodes) => [...prevNodes, newNode]);
   };
 
   return (
-    <div className="w-screen h-screen bg-gray-200 p-8">
-        <button
-        className="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
-        onClick={addColdMailNode}
-      >
-        Add Cold Email Node
-      </button>
+    <div className="w-full h-full bg-gray-200 p-8">
+     <Dropdown addColdMail={addColdMailNode} addDelay={addDelay} addLead={addLead} />
+     
+
+
+      <div className='h-[450px]'>
         <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        nodeTypes={nodeTypes}
-        edgeTypes={edgeTypes}
-        fitView
-      >
-        <Background />
-        <Controls />
-      </ReactFlow>
-      
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          nodeTypes={nodeTypes}
+          edgeTypes={edgeTypes}
+          fitView
+        >
+          <Background />
+          <Controls />
+        </ReactFlow>
+      </div>
     </div>
   );
 };
---
+++ mail-flow/src/components/Dropdown.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import useToggle from '../hooks/useToogle';
+
+
+const Dropdown = ({addLead,addColdMail,addDelay}) => {
+  const [dropdown,setDropdown] = useToggle(false);
+
+  return (
+    <div  ><button id="dropdownDefaultButton" onClick={setDropdown}  data-dropdown-toggle="dropdown" className=" z-10 flex flex-row items-center justify-center shadow w-44 h-10 text-white bg-blue-700 hover:bg-blue-800 rounded-lg text-sm text-center " type="button">Add Node
+    <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <path stroke="currentColor" strokeLinecap="round " strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+    </svg>
+    </button>
+    <div id="dropdownDelay" className={`z-10 absolute ${!dropdown && `hidden`} bg-white divide-y divide-gray-100 rounded-lg shadow w-44 `}>
+    <ul className="py-2 text-sm text-black ">
+      <li>
+        <button onClick={addLead} className="block w-full px-4 py-2 hover:outline-double">Lead Source</button>
+      </li>
+      <li>
+        <button onClick={addColdMail}  className="block  w-full px-4 py-2 hover:outline-double">Cold Email      </button>
+      </li>
+      <li>
+        <button onClick={addDelay}  className="block  w-full px-4 py-2 hover:outline-double ">Delay</button>
+      </li>
+    
+    </ul>
+</div>
+    </div>
+  )
+}
+
+export default Dropdown
